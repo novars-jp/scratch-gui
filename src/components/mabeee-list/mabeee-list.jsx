c63203f5142a8c32ff739e4ede2604c6100126b3
itome <t.t.itm.0403@gmail.com>
2018-03-18 15:03:45 +0900

    Add mabeee-list

---
+++ src/components/mabeee-list/mabeee-list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Box from '../box/box.jsx';
+import styles from './mabeee-list.css';
+
+const MaBeeeList = () => (
+    <Box className={styles.mabeeeList}>
+        MaBeeeList
+    </Box>
+);
+
+export default MaBeeeList;
