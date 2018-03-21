a01495f892245123e02575c802007da296f7ff67
itome <t.t.itm.0403@gmail.com>
2018-03-21 19:34:51 +0900

    Implement mabeee-list design and add mabeee-item

--- src/components/mabeee-list/mabeee-list.jsx
+++ src/components/mabeee-list/mabeee-list.jsx
@@ -2,12 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Box from '../box/box.jsx';
+import MaBeeeItem from '../mabeee-item/mabeee-item.jsx';
+import ActionMenu from '../action-menu/action-menu.jsx';
 import styles from './mabeee-list.css';
+import mabeeeIcon from './mabeeeIcon.svg';
 
-const MaBeeeList = () => (
+const MaBeeeList = ({ devices, onAddClick, onDeleteClick }) => (
     <Box className={styles.mabeeeList}>
-        MaBeeeList
+        <div className={styles.header}>
+            <div className={styles.headerTitle}>MaBeee</div>
+        </div>
+        <Box className={styles.scrollWrapper}>
+            <Box className={styles.deviceList}>
+                {devices.map(device => (
+                    <MaBeeeItem
+                        device={device}
+                        onDeleteClick={onDeleteClick}/>
+                ))}
+            </Box>
+        </Box>
+        <ActionMenu className={styles.addButton}
+            img={mabeeeIcon}
+            onClick={onAddClick}
+            title="Coonnect MaBeee"
+        />
     </Box>
 );
+
+MaBeeeList.propTypes = {
+    devices: PropTypes.array.isRequired,
+    onAddClick: PropTypes.func.isRequired,
+    onDeleteClick: PropTypes.func.isRequired
+}
 
 export default MaBeeeList;
